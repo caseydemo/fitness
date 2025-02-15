0dae58cd1bc98ce7237da1bd0d3dd0e10583b8b7
Casey DeMattina <cdemattina@gmail.com>
2025-02-15 10:09:45 -0500

    handleFormChange

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { ExerciseType } from "./types";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 
 import { getExercises, createExercise } from "./actions/exercise";
+import Card from "./components/Card";
 
 export default function Home() {
 	const [exercises, setExercises] = useState([] as ExerciseType[]);
@@ -11,13 +12,25 @@ export default function Home() {
 		name: "",
 		description: "",
 	});
+	const form = useRef<HTMLFormElement | null>(null);
 
 	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
-		// call the action to create an exercise
-        const newExercise = await createExercise(formData);
-        // add the new exercise to the list of exercises
-        setExercises([...exercises, newExercise]);
+		try {
+			if (!formData.name || !formData.description) {
+				throw new Error("missing input!");
+			}
+			const newExercise = await createExercise(formData); // call the action to create an exercise
+			setExercises([...exercises, newExercise]); // add the new exercise to the list of exercises
+			setFormData({ name: "", description: "" }); // clears out the inputs
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const handleFormChange = (e) => {
+		const { name, value } = e.target;
+		setFormData({ ...formData, [name]: value });
 	};
 
 	// on initial page load call a user action to get the exercises
@@ -29,43 +42,42 @@ export default function Home() {
 	}, []);
 
 	return (
-		<main>
+		<main className="">
 			<h1>Home</h1>
 
 			<h2>Exercise Input</h2>
-			<form onSubmit={handleSubmit}>
-				<div>
-					<label>
-						Name:
-						<input
-							type='text'
-							value={formData.name}
-							onChange={(e) =>
-								setFormData({
-									...formData,
-									name: e.target.value,
-								})
-							}
-						/>
-					</label>
-					<br />
-					<label>
-						Description:
-						<input
-							type='text'
-							value={formData.description}
-							onChange={(e) =>
-								setFormData({
-									...formData,
-									description: e.target.value,
-								})
-							}
-						/>
-					</label>
-					<br />
-					<button type='submit'>Submit</button>
-				</div>
-			</form>
+
+			<div className="">
+				<form ref={form} onSubmit={handleSubmit}>
+					<div className="exercise_form_wrapper">
+						<label className="input-group-text">
+							Name:
+							<input
+								className="form-control"
+								type="text"
+								value={formData.name}
+								name="name"
+								onChange={handleFormChange}
+							/>
+						</label>
+
+						<label className="input-group-text">
+							Description:
+							<input
+								className="form-control"
+								type="text"
+								value={formData.description}
+								name="description"
+								onChange={handleFormChange}
+							/>
+						</label>
+
+						<button type="submit" className="btn btn-primary">
+							Submit
+						</button>
+					</div>
+				</form>
+			</div>
 
 			<h3>Exercises</h3>
 			{/* add loading state */}
@@ -78,22 +90,24 @@ export default function Home() {
 				</div>
 			)}
 			{exercises && exercises.length > 0}
-			<table>
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Description</th>
-					</tr>
-				</thead>
-				<tbody>
-					{exercises.map((exercise) => (
-						<tr key={exercise.id}>
-							<td>{exercise.name}</td>
-							<td>{exercise.description}</td>
+			<Card title="exercises">
+				<table className="table table-dark table-striped">
+					<thead>
+						<tr>
+							<th scope="col">Name</th>
+							<th scope="col">Description</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
+					</thead>
+					<tbody>
+						{exercises.map((exercise) => (
+							<tr key={exercise.id}>
+								<td>{exercise.name}</td>
+								<td>{exercise.description}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</Card>
 		</main>
 	);
 }
