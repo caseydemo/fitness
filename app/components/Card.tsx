92134b83d6246fade772cfd97bd5d476240ad643
Casey DeMattina <cdemattina@gmail.com>
2025-02-15 10:37:40 -0500

    width and margin to card

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,10 +1,10 @@
+import styles from "./card.module.css"
 export default function Card(props: any) {
     const {title, children} = props;
 	return (
-		<div className="card" >
+		<div className={styles.card} >
 			<div className="card-body">
 				<h5 className="card-title">{title}</h5>
-				<h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
 				<p className="card-text">
 					{children}
 				</p>
