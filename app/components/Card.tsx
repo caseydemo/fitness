5eb66e548ef5b98444585ca24925e07724f8b314
Casey DeMattina <cdemattina@gmail.com>
2025-02-16 00:50:46 -0500

    extraClasses

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,8 +1,10 @@
 import styles from "./card.module.css"
 export default function Card(props: any) {
-    const {title, children} = props;
+    const {title, extraClasses, children} = props;
+
+    const classes = extraClasses ? extraClasses : '';
 	return (
-		<div className={styles.card} >
+		<div className={`${styles.card} ${classes}`} >
 			<div className="card-body">
 				<h5 className="card-title">{title}</h5>
 				<p className="card-text">
