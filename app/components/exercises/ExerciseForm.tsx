8168fd96d1b694d7a9abde9a0ebd0512c584bf26
Casey DeMattina <cdemattina@gmail.com>
2025-02-19 08:29:55 -0500

    improving button/input styles form

--- app/components/exercises/ExerciseForm.tsx
+++ app/components/exercises/ExerciseForm.tsx
@@ -1,46 +1,52 @@
-export default function ExerciseForm(props: any) {
-    const { handleSubmit, formRef, formData, handleFormChange } = props;
-    return (
-        <>        
-        <form ref={formRef} onSubmit={handleSubmit}>
-					<div className="container">
-						<div className="row">
-							<div className="col-sm">
-								<label className="input-group-text">
-									Name:
-									<input
-										className="form-control"
-										type="text"
-										value={formData.name}
-										name="name"
-										onChange={handleFormChange}
-									/>
-								</label>
-							</div>
+import styles from './form-styles.module.css';
 
-							<div className="col-sm">
-								<label className="input-group-text">
-									Description:
-									<input
-										className="form-control"
-										type="text"
-										value={formData.description}
-										name="description"
-										onChange={handleFormChange}
-									/>
-								</label>
-							</div>
+export default function ExerciseForm(props: any) {
+	const { handleSubmit, formRef, formData, handleFormChange } = props;
+	return (
+		<div className='form__container'>
+			<form
+				ref={formRef}
+				onSubmit={handleSubmit}
+			>
+				<div className='container'>
+					<div className='row'>
+						<div className='col'>
+							<label className='input-group-text'>
+								Name:
+								<input
+									className='form-control'
+									type='text'
+									value={formData.name}
+									name='name'
+									onChange={handleFormChange}
+								/>
+							</label>
 						</div>
-						<div className="row">
+
+						<div className='col'>
+							<label className='input-group-text'>
+								Description:
+								<input
+									className='form-control'
+									type='text'
+									value={formData.description}
+									name='description'
+									onChange={handleFormChange}
+								/>
+							</label>
+						</div>
+
+						<div className={`col ${styles.exercise_submit_button_container}`}>
 							<button
-								type="submit"
-								className="btn btn-primary"
+								type='submit'
+								className='btn btn-sm btn-primary'
 							>
 								Submit
 							</button>
 						</div>
 					</div>
-				</form>
-        </>
-    )
+				</div>
+			</form>
+		</div>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,4 @@
-"use client";
-import { ExerciseType } from "./types";
-import { useEffect, useRef, useState } from "react";
 import Exercises from "./components/exercises/Exercises";
-
-import { getExercises, createExercise, updateExercise, deleteExercise } from "./actions/exercise";
-import Card from "./components/Card";
 
 export default function Home() {
 
